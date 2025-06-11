a1f8d232e269924c93cbbb1e68194cb803b95045
jinmu0410 <1922295863@qq.com>
2025-06-11 17:00:43 +0800

    feat: add ethereum init connector method

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/jinmu/go-blockchain/internal/blockchain/ethereum"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	client := ethereum.GetClient()
+
+	fmt.Printf("client: %v\n", client)
+
+	
 }
